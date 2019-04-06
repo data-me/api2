7a4be8395497bc56ac769313f92d0e4114717b47
Waco <wacomacaco@gmail.com>
2019-04-06 03:06:18 +0200

    Listado de Submitions y aceptarlas/rechazarlas

--- mysite/datame/submition.py
+++ mysite/datame/submition.py
@@ -23,6 +23,19 @@ class Submition_view(APIView):
             return res
         except Exception as e:
             return JsonResponse({"message":"Oops, something went wrong" + str(e)})
+    def get(self, request, format=None):
+        try:
+            user_logged = User.objects.all().get(pk = request.user.id)
+            if (user_logged.groups.filter(name='Company').exists()):
+                    thisCompany = Company.objects.all().get(user = request.user)
+                    submitions = Submition.objects.filter(offer__company = thisCompany).values('dataScientist_id','offer_id','comments','file','status','offer__title', 'id')
+                    return JsonResponse(list(submitions), safe=False)
+            elif(user_logged.groups.filter(name='DataScientist').exists()):
+                    dataScientistRecuperado = DataScientist.objects.all().get(user = request.user)
+                    submitions = Submition.objects.filter(dataScientist = dataScientistRecuperado).values('dataScientist_id','offer_id','comments','file','status','offer__title', 'id')
+                    return JsonResponse(list(submitions), safe=False)
+        except Exception as e:
+            return JsonResponse({"message":"Oops, something went wrong" + str(e)})
         
 class Check_submition(APIView):
     permission_classes = (IsAuthenticated,)
@@ -40,5 +53,25 @@ class Check_submition(APIView):
                 if (apply.status == 'AC' and apply.dataScientist == dataScientist and (not Submition.objects.all().filter(offer = apply.offer).exists())):
                     res = JsonResponse({"message": "true"}, safe = False)
             return res
+        except Exception as e:
+            return JsonResponse({"message":"Oops, something went wrong" + str(e)})
+        
+class Change_status(APIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request, format=None):
+        try:
+            data = request.POST
+            submitId = data['submitId']
+            status = data['status']
+            submit = Submition.objects.all().get(pk = submitId)
+            user_logged = User.objects.all().get(pk = request.user.id)
+            if ((not user_logged.groups.filter(name='Company').exists())):
+                return JsonResponse({"message": "Only the a company can do that"}, safe = False)
+            company = Company.objects.all().get(user = user_logged)
+            if(not submit.offer.company == company):
+                return JsonResponse({"message": "Only the owner company can do that"}, safe = False)
+            else:
+                Submition.objects.all().filter(pk = submitId).update(status = status)
+            return JsonResponse({"message": "Status changed"}, safe = False)
         except Exception as e:
             return JsonResponse({"message":"Oops, something went wrong" + str(e)})
